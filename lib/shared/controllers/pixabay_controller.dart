4411acd3f825dbe73af1fe3c2f9960f21e06acd9
gfalav <gfalav@hotmail.com>
2025-04-01 16:18:26 -0300

    fixed auth emulator error android

--- lib/shared/controllers/pixabay_controller.dart
+++ lib/shared/controllers/pixabay_controller.dart
@@ -14,7 +14,7 @@ class PixabayController extends GetxController {
     final dio = Dio();
     await dio
         .get(
-          'https://pixabay.com/api/?key=49226875-dd469dfea4c42634d1eec15d2&q=nature&orientation=vertical&min_width=800&min_height=1100&image_type=photo&per_page=80',
+          'https://pixabay.com/api/?key=49226875-dd469dfea4c42634d1eec15d2&q=motorcycles&orientation=vertical&min_width=800&min_height=1100&image_type=photo&per_page=80',
         )
         .then((result) {
           result.data['hits'].forEach((element) {
